a1da3bb6a63f441fb0c1ca43a5b7ed2f7f8d3597
Surowiec Krzysztof <krzysztof.surowiec@volvo.com>
2020-03-24 16:28:08 +0100

    Hello world project from the master class course.

---
+++ HelloWorld/escapechar.py
@@ -0,0 +1,12 @@
+splitString = "This string has been\nsplit\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting.')
+
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting.""")
+
+print(r"C:\Users\tim\notes.txt")
+print("C:\\Users\\tim\\notes.txt")
---
+++ HelloWorld/strings.py
@@ -0,0 +1,19 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+print('hello' + ' world')
+
+greeting = "Hello"
+name = "Krzysiek"
+print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = '2 years'
+print(type(age_in_words))
